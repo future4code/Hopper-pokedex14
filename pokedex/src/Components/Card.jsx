a8fba9e86ea4c38b8b62025a7616dbbab23c3e63
Leandro <leandro.ap.leite@gmail.com>
2022-06-29 18:27:28 -0300

    corrigindo

--- pokedex/src/Components/Card.jsx
+++ pokedex/src/Components/Card.jsx
@@ -1,7 +1,36 @@
-const Card = () => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Card.css"
+
+
+const Card = (props) => {
+
+    const navigate = useNavigate()
+
+    const [pokemon, setPokemon] = useState({})
+    const [img, setImg] = useState("")
+
+    const url = props.url
+
+    useEffect(() => {
+        axios.get(url).then((res) => {
+            setPokemon(res.data)
+            setImg(res.data.sprites.front_default)
+        }).catch((err) => {
+            console.log(err);
+        })
+    },[])
+
     return (
-        <>
-        </>
+        <div className="card">
+            <p>{props.nome}</p>
+            <img src={img} className="img"/>
+            <div>
+                <button>Adicionar ao Pokedex</button>
+                <button>Ver detalhes</button>
+            </div>
+        </div>
     )
 }
 
