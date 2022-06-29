8c12e0f6dd26896b7f8433e8aeea7ba329a536ff
MadsonPereira <madson.ribas@gmail.com>
2022-06-28 21:31:49 -0300

    Projeto Pokedex

---
+++ pokedex/src/Pages/Pokedex.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Components/Header";
+
+const Pokedex = () => {
+    return (
+        <>
+            <Header/>
+            <h1>Pokedex</h1>
+        </>
+    )
+}
+
+export default Pokedex;
